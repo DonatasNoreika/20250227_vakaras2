e7c43486633902135416a4e077944d40610dc5b7
DonatasNoreika <donoras@gmail.com>
2025-02-27 21:16:21 +0200

    Sukurtas įrašo pridėjimo puslapis, sutvarkytas meniu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -11,6 +11,14 @@ def home():
 @app.route("/zurnalas/")
 def zurnalas():
     return render_template("zurnalas.html", zurnalas=islaidu_zurnalas)
+
+@app.route("/prideti/", methods=['GET', 'POST'])
+def prideti_irasa():
+    if request.method == "POST":
+        suma = int(request.form['suma'])
+        islaidu_zurnalas.append(suma)
+        return redirect('/zurnalas/')
+    return render_template("prideti.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

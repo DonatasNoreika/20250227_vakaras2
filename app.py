ae07e68201bc584e81df96bf9d4b32c2888b0a62
DonatasNoreika <donoras@gmail.com>
2025-02-27 20:56:39 +0200

    Sukurtas žurnalo įrašų puslapis

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+islaidu_zurnalas = [700, -20, -30]
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/zurnalas/")
+def zurnalas():
+    return render_template("zurnalas.html", zurnalas=islaidu_zurnalas)
 
 if __name__ == "__main__":
     app.run(debug=True)
